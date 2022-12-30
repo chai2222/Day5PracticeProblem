21be85cd4395a05c7c5b754b9e3cf3701983d169
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 16:30:56 +0530

    PrimeFactor

---
+++ PrimeFactor/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PrimeFactor
+{
+    class PrimeFactor
+    {
+        public static void readData()
+        {
+            Console.WriteLine("Enter The Number to Find The Prime Factor");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i < number; i++)
+            {
+                while (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                    number = number / i;
+                }
+            }
+            if (number > 2)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrimeFactor.readData();
+        }
+    }
+}
