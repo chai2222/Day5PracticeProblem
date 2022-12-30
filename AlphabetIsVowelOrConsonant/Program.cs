5a7a6dc76e41c786244da28a3dd067eb81ce3652
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 16:36:55 +0530

    AlphabetIsVowelOrConsonant

---
+++ AlphabetIsVowelOrConsonant/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AlphabetIsVowelOrConsonant
+{
+    class Character
+    {
+        public static void Display()
+        {
+            char ch;
+          
+            Console.WriteLine("check whether the input alphabet is a vowel or not:\n");
+            Console.WriteLine("-----------------------------------------------------");
+           
+
+            Console.Write("Input an Alphabet (A-Z or a-z) : ");
+            ch = Convert.ToChar(Console.ReadLine().ToLower());
+            int i = ch;
+            if (i >= 48 && i <= 57)
+            {
+                Console.Write("You entered a number, Please enter an alpahbet.");
+            }
+            else
+            {
+                switch (ch)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is not a vowel");
+                        break;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Character.Display();
+        }
+    }
+}
