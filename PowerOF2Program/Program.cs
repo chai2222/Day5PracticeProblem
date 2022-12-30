d17e70586d30ac3b6bbf3fc9d574fd06370d4ea1
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 16:27:30 +0530

    PowerOF2

---
+++ PowerOF2Program/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PowerOF2Program
+{
+    class PowerofNumber
+    {
+        public static void readData()
+        {
+            int mul = 1;
+
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // CONDITION CHECK
+            if (number > 31)
+            {
+                Console.WriteLine("value of number should be less than 31");
+                return;
+            }
+
+            // COMPUTATION
+            else
+            {
+                Console.WriteLine(mul);
+                for (int j = 1; j <= number; j++)
+                {
+                    mul = mul * 2;
+                    Console.WriteLine(mul);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PowerofNumber.readData();
+        }
+    }
+}
