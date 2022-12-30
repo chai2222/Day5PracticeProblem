449e5d4ca4f45a630043a667328e3e4c39e97a57
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 16:35:25 +0530

    NumberEvenOrOdd

---
+++ NumberEvenOrOdd/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace NumberEvenOrOdd
+{
+    class EvenOdd
+    {
+        public static void DisplayNumber()
+        {
+            int Number;
+
+            Console.WriteLine("Enter The Number");
+            Number = Convert.ToInt32(Console.ReadLine());
+
+            if (Number % 2 == 0)
+            {
+                Console.WriteLine("Number Is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number Is Odd");
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EvenOdd.DisplayNumber();
+        }
+    }
+}
+
