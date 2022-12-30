3d7b96217ef0c33416474c915804e9fcd39acf42
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 16:33:53 +0530

    SwapTwoNumber

---
+++ SwapTwoNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace SwapTwoNumber
+{
+    class Swap
+    {
+        public static void DisplayNumber()
+        {
+            int a, b, c;
+
+            Console.WriteLine("Enter The First Number A");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter The Second Number B");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before The Swapping Number Is: " + "a= " + a + " " + "b= " + b);
+            //By Using Third Variable
+            /* c = a;
+             a = b;
+             b = c;
+            */
+            //By WithOut Using Thrid Variable
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+
+            Console.WriteLine("After The Swapping Number Is: " + "a= " + a + " " + "b= " + b);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Swap.DisplayNumber();
+        }
+    }
+}
