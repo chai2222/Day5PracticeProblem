d20edcc214e1bf7175ec8b795a90684fc463e7b3
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 16:29:17 +0530

    HarmonicNumber

---
+++ HarmonicNumberProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HarmonicNumberProblem
+{
+    class Number
+    {
+        public static void readData()
+        {
+            double s = 0.0;
+
+            Console.WriteLine("Input the number of terms : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write("1/" + i + " + ");
+                s = s + 1 / (float)i;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum of Series upto" + number + " terms :" + s);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Number.readData();
+        }
+    }
+}
