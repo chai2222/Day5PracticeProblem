9c0eb05a828e1e4a2bf2ecf4ed68804e05ca2589
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 16:32:22 +0530

    ComputeQuotientAndRemainder

---
+++ ComputeQuotientAndRemainder/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CompueQuotientAndReminder
+{
+    class Division
+    {
+        public static void Display()
+        {
+            int dividend = 0;
+            Console.WriteLine("Enter the Dividend ");
+            dividend = Convert.ToInt32(Console.ReadLine());
+
+            int divisor = 0;
+            Console.WriteLine("Enter the Divisor ");
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+
+        }
+        
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Division.Display();
+        }
+    }
+}
